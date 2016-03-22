9d93272ee7a45eb01b2638f990fd11ada2cb70a9
Thomas Riis <thomasoriis@hotmail.com>
2016-03-22 12:30:05 +0100

    First commit

---
+++ Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.QueryString.HasKeys())
+        {
+            foreach (String key in Request.QueryString.AllKeys)
+            {
+                Label_Info.Text += "Key: " + key + " Value: " + Request.QueryString[key] + "<br />";
+            }
+        }
+    }
+}
